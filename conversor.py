1dea9b4105106b977f513b00045bdbb5547b6f00
joao-manjabosco <160279939+joao-manjabosco@users.noreply.github.com>
2024-09-10 11:05:16 -0300

    Update conversor.py

--- conversor.py
+++ conversor.py
@@ -1,48 +1,32 @@
 from decimal import Decimal
 
+
 # Função para processar os dados OFX
 def process_ofx(ofx_data):
-    account = ofx_data.account
-
-    conta = [{
-        'numero da conta': account.account_id,
-        'numero_roteamento_banco': account.routing_number,
-        'agencia': account.branch_id,
-        'tipo_conta': account.type
-    }]
-
-    institution = account.institution
-    instituicao = [{
-        'instituicao_banco': institution.organization
-    }]
-
-    statement = account.statement
-    statem = [{
-        'start_date': statement.start_date.isoformat() if statement.start_date else None,
-        'end_date': statement.end_date.isoformat() if statement.end_date else None,
-        'balance': float(statement.balance) if isinstance(statement.balance, Decimal) else statement.balance
-    }]
-
-    transact = []
-    for transaction in statement.transactions:
-        transact.append({
-            'payee': transaction.payee,
-            'type': transaction.type,
-            'date': transaction.date.isoformat() if transaction.date else None,
-            'user_date': transaction.user_date.isoformat() if transaction.user_date else None,
-            'amount': float(transaction.amount) if isinstance(transaction.amount, Decimal) else transaction.amount,
-            'id': transaction.id,
-            'memo': transaction.memo,
-            'sic': transaction.sic,
-            'mcc': transaction.mcc,
-            'checknum': transaction.checknum
-        })
-
-    resultado = {
-        'instituicao': instituicao,
-        'conta': conta,
-        'extrato': statem,
-        'transacoes': transact
+    json = {
+        "Instituicao banco": ofx_data.account.institution.organization,
+        "Agencia": ofx_data.account.routing_number,                                                    # O número de roteamento do banco
+        "Tipo de conta": ofx_data.account.type,                                                        # Um objeto Tipo de conta
+        "Numero da conta": ofx_data.account.account_id,                                                # O número da conta
+        "Start date": ofx_data.account.statement.start_date.strftime('%d/%m/%Y %H:%M:%S'),             # A data de início das transações
+        "End date": ofx_data.account.statement.end_date.strftime('%d/%m/%Y %H:%M:%S'),                 # A data final das transações
+        "balance (saldo)": float(ofx_data.account.statement.balance),                                  # O dinheiro na conta na data do extrato
+        "transactions": [
+            {
+                'payee (beneficiario)': transaction.payee,                                                                                 # payee: O nome do beneficiário ou destinatário do pagamento (quem recebeu o valor).
+                'type ()': transaction.type,                                                                                               # type: O tipo de transação, como crédito, débito ou transferência.
+                'date': transaction.date.strftime("%d/%m/%Y"),                                                                             # Converter para string. A data em que a transação foi registrada pelo banco.
+                'user_date': transaction.user_date.strftime("%d/%m/%Y") if transaction.user_date else None,                                # Converter para string. user_date: A data que o usuário associou à transação, que pode ser diferente da data oficial registrada pelo banco.
+                'amount (valor_transacao)': float(transaction.amount) if isinstance(transaction.amount, Decimal) else transaction.amount,  # Converter para float. amount: O valor da transação (positivo para créditos, negativo para débitos).
+                'id': transaction.id,                                                                                                      # id: Um identificador único da transação.
+                'memo (OBS)': transaction.memo,                                                                                            # memo: Um campo para observações ou notas associadas à transação.
+                'sic': transaction.sic,                                                                                                    # sic: Standard Industrial Classification (SIC) code, que identifica a indústria da empresa envolvida na transação.
+                'mcc': transaction.mcc,                                                                                                    # mcc: Merchant Category Code (MCC), que categoriza o tipo de serviço ou negócio associado à transação.
+                'checknum': transaction.checknum                                                                                           # checknum: O número do cheque, caso a transação tenha sido feita por cheque.
+            }
+            for transaction in ofx_data.account.statement.transactions
+        ]
     }
 
-    return resultado
+    return json
+
